f698f4a1a46fac264765d65e7c4ccf818fa6f3a3
taher-kathiriya <taher@htree.plus>
2022-08-24 11:01:21 +0530

    add commit

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from routers import blog, user, authentication
 app = FastAPI()
 models.Base.metadata.create_all(engine)
 
+# Setup route files
 app.include_router(authentication.router)
 app.include_router(blog.router)
 app.include_router(user.router)
